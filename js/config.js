74c7eee645a4055f30ea2a3ccc6c650251b5fc1c
Harshini G <harshiniguhan19@gmail.com>
2026-02-12 13:52:06 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-south-1_tBpNh9GeD',
-        userPoolClientId: '3tr0h4ek70a80u0s2o045j2m4q',
+        userPoolClientId: '1vr3nbencepga6bdphc24gl4go3pm6qbqfmok07osv4i101e1a7f',
         region: 'ap-south-1'
     },
     api: {
@@ -11,6 +11,7 @@ window._config = {
         clientId: '984566112203-ap9e58gecioe9csom4mej6vlae4hjtj3.apps.googleusercontent.com' // <-- Add this
     }
 };
+
 
 
 
