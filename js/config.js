df4a18dc7de79f4ad07b56b7add69555a21f5c95
Harshini G <harshiniguhan19@gmail.com>
2026-02-12 18:05:57 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,26 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_y578h0eJS',
+        userPoolClientId: 'T2qduv81mEjfnsob4pgmgpbf',
+        region: 'ap-south-1'
+    },
+    api: {
+        invokeUrl: 'https://huf3ap2prb.execute-api.ap-south-1.amazonaws.com/prods'
+    },
+    google: {
+        clientId: '984566112203-ap9e58gecioe9csom4mej6vlae4hjtj3.apps.googleusercontent.com' // <-- Add this
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
